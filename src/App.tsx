2de2e25c9f9c79e0ab076e933d398b6c2c5cb43e
Casey Hyde <casey@fidgetlabs.co>
2023-06-16 16:02:13 -0500

    Fuckity fuck fuck

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Logo from './Logo';
 import './App.css';
 
-
 const App: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [validPhoneNumber, setValidPhoneNumber] = useState(false);
+
+  const handlePhoneNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handlePhoneNumberSubmit = () => {
+    // Perform validation on the phone number here
+    setValidPhoneNumber(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Logo />
+      <header className="App-header title-container">
         <h1>
-          <span className="standard-text">Watch Something </span>
-          <span className="exciting-text">Awesome</span>
+          <span className="standard-text">Movies with your</span>
         </h1>
         <h1>
-          <span className="standard-text">With Someone </span>
-          <span className="exciting-text">Awesome</span>
+          <span className="exciting-text">Favorite</span>
         </h1>
-        <h2>Prepare to enjoy a movie with Casey!</h2>
+        <h1>
+          <span className="standard-text">People</span>
+        </h1>
+        {!validPhoneNumber && (
+          <button onClick={handlePhoneNumberSubmit} className='button-no-blur'>Start Watching</button>
+        )}
       </header>
 
-
-      <section className="App-section">
-      <img className="Section-image" src="big-eye.jpg" alt="Get Started" />
-        <div className="Section-content">
-          <h3>Get Started</h3>
-          <p>What kind of movie watcher are you? Choose your favorite genres and preferences so Casey can suggest the best movies for you to watch together.</p>
-        </div>
-      </section>
-
-      <section className="App-section">
-        <img className="Section-image" src="big-smile-monster.jpg" alt="Movie Essentials" />
-        <div className="Section-content">
-          <h3>Movie Essentials</h3>
-          <p>Pick the right movie snacks! The best part of watching a movie is the snacks. Let Casey guide you through picking the perfect snacks for your movie night.</p>
-        </div>
-      </section>
-
-      <section className="App-section">
-        <img className="Section-image" src="box-pattern.jpg" alt="Understand Your Movie & Timing" />
-        <div className="Section-content">
-          <h3>Understand Your Movie & Timing</h3>
-          <p>Understand the plot and timing. Casey will provide a quick summary of the movie and the best time to start watching to accommodate different time zones.</p>
-        </div>
-      </section>
-
-      <footer className="App-footer">
-        <p>Get ready to embark on a movie adventure with Casey!</p>
-      </footer>
+      <div className={`Content-wrapper ${validPhoneNumber ? '' : 'blurred'} content`}>
+        <section className="App-section">
+          <img className="Section-image" src="big-eye.jpg" alt="Get Started" />
+          <div className="Section-content">
+            <h3>Get Started</h3>
+            <p>What kind of movie watcher are you? Choose your favorite genres and preferences so Casey can suggest the best movies for you to watch together.</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Logo.tsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div className="logo-container">
+      <h3>Watch a Movie with Casey</h3>
+    </div>
+  );
+}
+
+export default Logo;
